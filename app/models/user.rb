9b67853d809129a66dbddceb47a567133bcc80ec
YukoOkayasu <yuko.cc.09@gmail.com>
2019-04-17 16:00:01 +0900

    Edit index.css

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,9 @@ class User < ApplicationRecord
   has_many :carts
 
   mount_uploader :image, ImageUploader
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true
 end
